20346f3ba637d44d1db7a7a58bb84b031457765d
taoufiq <taoufiq.benmessaoud@gmail.com>
2020-05-31 12:27:13 +0100

    Project api

---
+++ gestion-stock-api/src/main/java/com/fst/gestionstockapi/GestionStockApiApplication.java
@@ -0,0 +1,13 @@
+package com.fst.gestionstockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionStockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionStockApiApplication.class, args);
+	}
+
+}
---
+++ gestion-stock-api/src/test/java/com/fst/gestionstockapi/GestionStockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fst.gestionstockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionStockApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
