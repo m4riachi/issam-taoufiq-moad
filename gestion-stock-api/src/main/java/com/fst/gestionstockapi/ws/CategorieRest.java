f9b8864a1808715a6cec941cd3e6eaf80739b160
moad <moad@192.168.1.105>
2020-06-03 20:22:23 +0100

    Changement Categorie et Produit : WS Rest

--- gestion-stock-api/src/main/java/com/fst/gestionstockapi/ws/CategorieRest.java
+++ gestion-stock-api/src/main/java/com/fst/gestionstockapi/ws/CategorieRest.java
@@ -1,5 +1,47 @@
 package com.fst.gestionstockapi.ws;
 
-public class CategorieRest {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fst.gestionstockapi.bean.Categorie;
+import com.fst.gestionstockapi.service.CategorieService;
+
+@RestController
+@RequestMapping()
+@CrossOrigin()
+public class CategorieRest {
+	
+	@Autowired
+	private CategorieService categorieService;
+	
+	@PostMapping("/")
+	public int save (@RequestBody Categorie categorie) {
+		return categorieService.save(categorie);
+	}
+	
+	@DeleteMapping("/id/{id}")
+	public int delete(@PathVariable Long id) {
+		return categorieService.delete(id);
+	}
+	
+	@PutMapping("/")
+	public int update(@RequestBody Categorie categorie) {
+		return categorieService.update(categorie);
+	}
+	
+	@GetMapping("/")
+	public List<Categorie> findAll(){
+		return categorieService.getAll();
+	}
+	
 }
--- gestion-stock-api/src/main/java/com/fst/gestionstockapi/ws/ProduitRest.java
+++ gestion-stock-api/src/main/java/com/fst/gestionstockapi/ws/ProduitRest.java
@@ -1,5 +1,45 @@
 package com.fst.gestionstockapi.ws;
 
-public class ProduitRest {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fst.gestionstockapi.bean.Produit;
+import com.fst.gestionstockapi.service.ProduitService;
+
+@RestController
+@RequestMapping()
+@CrossOrigin()
+public class ProduitRest {
+	@Autowired
+	private ProduitService produitService;
+	
+	@PostMapping("/")
+	public int save(@RequestBody Produit produit) {
+		return produitService.save(produit);
+	}
+	
+	@DeleteMapping("/id/{id}")
+	public int delete(@PathVariable Long id) {
+		return produitService.delete(id);
+	}
+	
+	@PutMapping("/")
+	public int update (@RequestBody Produit produit) {
+		return produitService.update(produit);
+	}
+	
+	@GetMapping("/")
+	public List<Produit> findAll(){
+		return produitService.getAll();
+	}
 }
